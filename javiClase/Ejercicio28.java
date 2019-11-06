8e9c17242a49fdfc6d72b0b956ae0e4b64447160
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-06 19:20:46 +0100

    sss

---
+++ javiClase/Ejercicio28.java
@@ -0,0 +1,14 @@
+package javiClase;
+import java.util.Scanner;
+public class Ejercicio28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
+		float radians, sine,cosine,tangente;
+		radians=30*Math
+		System.out.printf("Degrees \tRadians \tSine \tCosine \tTangente");
+		System.out.printf("30\t\t  %f\t\t %f\t\t  %f\t\t %f\t\t", radians,sine,cosine,tangente);
+	}
+
+}
