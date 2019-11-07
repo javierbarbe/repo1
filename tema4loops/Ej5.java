b4099772c313d51c7f11cdadbc6630179c1493aa
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-07 17:56:06 +0100

    Add files via upload
    4 y 5 a medias

---
+++ tema4loops/Ej5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ej5 {
+
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		int num, max;
+		num=entrada.nextInt();
+		max=num;
+		while (num!=0) {
+			num=entrada.nextInt();
+		}
+	}
+
+}
