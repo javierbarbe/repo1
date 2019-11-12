cff2bda7f47450a32b980f466baad4d9de96678d
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-12 14:10:27 +0100

    11-13

---
+++ tema4loops/Ej13berrorescodigo.java
@@ -0,0 +1,30 @@
+package loopings4;
+
+public class Ej13berrorescodigo {
+//que lo resuelva el profe, no entiendo 
+	public static void main(String[] args) {
+		int i, j=0; 
+		//
+		for ( i =0; i< 10; i++) {
+		
+		if (i<j) {
+			System.out.println("aqui empieza i " +i);
+		}else {
+			System.out.println(j);
+		}
+	while (10>j) {//systemoutprint es una aclaracion
+			j++;
+		System.out.println("esta es j+1 "+j);
+			}
+		do {
+			//j++;//systemoutprint es una aclaracion
+			System.out.println("esta es el valor maximo de j  "+j);
+			j++;
+		}while(j<10);//imprime y luego comprueba para 
+						//salir del bucle
+		}
+		}
+		}
+	
+
+
