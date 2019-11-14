7a40e0306667b85bc04a94189ce0dbc6facf8acd
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-14 18:09:05 +0100

    tema 3 n

---
+++ tema3/Rectangulo.java
@@ -0,0 +1,15 @@
+package cocheClasePaquete;
+
+public class Rectangulo {
+
+	//atributos
+	int largo, ancho;
+	//constructor 
+	public Rectangulo() {
+		
+	}
+	public Rectangulo(int lg, int anch) {
+		
+		
+	}
+}
