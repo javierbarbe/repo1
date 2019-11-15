74b1765e2853169d84c19ea7c7363d997508aceb
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-15 14:07:54 +0100

    14

---
+++ tema4loops/Ej14.java
@@ -0,0 +1,19 @@
+
+public class Ej14 {
+
+	public static void main(String[] args) {
+		// 
+		 int i=0;
+		 do {
+		 	System.out.println( i+4);
+		 	i++;} 
+		 while(i<10);
+		 	
+		 for (int h=0; h<10;h++) {
+		 	System.out.println(h+4);
+		 }
+		 
+		 	}
+	}
+
+
