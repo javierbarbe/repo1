69d5471b900cd23469a026efcd1b61211660fb8e
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-11-20 13:28:55 +0100

    Update Ej12.java

--- tema4loops/Ej12.java
+++ tema4loops/Ej12.java
@@ -4,11 +4,22 @@ public class Ej12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int count=0, // no se realiza puesto que no hay declarada n;
-while(count<n) {
+while(count<n) { // se realizaria n-1 veces
 	
 	count++;
 }
-//no se realiza ninguno de los bucles porque no esta definida n;
+		//b
+		for (int count=0; count<=n; count++){} // se realizaria n veces
+//c
+		int count=5; 
+		while(count<n){
+			count++;} // se realizaria n-6 veces
+//d
+		int count=5; //va añadiendo 3 a count cada vez, por lo que se imprimira 
+				// (n-5)/3
+		while(count<n){
+			count=count+3;
+		}
 
 	}
 
