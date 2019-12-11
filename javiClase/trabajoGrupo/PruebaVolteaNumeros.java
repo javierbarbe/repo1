8b658d3872a0c349705429391fa8be9507c899b4
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2019-12-11 15:17:16 +0100

    voltea y capicua

---
+++ javiClase/trabajoGrupo/PruebaVolteaNumeros.java
@@ -0,0 +1,13 @@
+package trabajoGrupo;
+
+public class PruebaVolteaNumeros {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		volteaNumeros per= new volteaNumeros();
+		per.binarioDecimal();
+		
+	}
+
+}
