c02d3ad3756540b3f6b44597ee732dd919410638
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2020-02-04 13:19:44 +0100

    cuenyaletras repe ok

--- tema6/Ej1.java
+++ tema6/Ej1.java
@@ -29,21 +29,56 @@ public class Ej1 {
 		 * letra en tu String
 		 */
 		
+		char almacen[]=new char[15];
 		char abecedario []= new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-	
+	//int letrasFinales=0;
 	pala.toLowerCase();
 	for (int i=0;i<pala.length();i++) {
-		int cuentaLetra=0;
+		//int cuentaLetra=0;
 		letra=pala.charAt(i);
 		for(int j=0;j<abecedario.length;j++) {
 			if(abecedario[j]==pala.charAt(i)) {
-				cuentaLetra++;
-				System.out.println("De la letra "+ abecedario[j]+ " hay "+cuentaLetra);
+				//int cambio=0;
+				
+				for (int p=0;p<almacen.length;p++) {
+					almacen[i]=letra;				
+			  }
+				
 			}
 		}
 		
 	}
 	
-	
+	for(int i=0;i<almacen.length;i++) {
+		int cambio;
+		int cuentaLetra=0;
+		for(int j=0; j<almacen.length;j++) {
+			cambio=0;
+				if (cambio==0){
+					 
+					if(almacen[i]==almacen[j]) {
+						cuentaLetra++;
+						//letrasFinales++;
+						cambio=1;
+					}else {
+						//almacen[i]=letra;
+						//cuentaLetra++;
+						cambio=1;
+						//letrasFinales+=cuentaLetra;
+					}
+				}
+		} System.out.println("de la letra " +almacen[i]+ " hay "+ cuentaLetra);
 	}
+	
+	
+	}// fin abecedario
+	
+	static void ordenaArrays() {
+		
+	} // fin de ordena Arrays
+	
+	
+	
+	
+	
 }
