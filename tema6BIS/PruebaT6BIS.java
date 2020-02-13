ac588ab38ca4fa15d34e8997051638e1b30874e3
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2020-02-13 13:41:05 +0100

    Add files via upload

---
+++ tema6BIS/PruebaT6BIS.java
@@ -0,0 +1,19 @@
+package tema6BIS;
+
+public class PruebaT6BIS {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Apéndice de método generado automáticamente
+//		Ej1 ne= new Ej1();
+//		ne.imprime();
+//		Ej4 ni = new Ej4();
+//		ni.rellenaYcalculaSumaFila();
+//		ni.imprimeYcalculaSumaColumnas();
+//		Ej6 no= new Ej6();
+//		no.iniciaYmuestraMayorMenor();
+		Ej7 map=new Ej7();
+	//	map.imprimeMapa();
+		map.pideCoordenadas();
+	}
+
+}
