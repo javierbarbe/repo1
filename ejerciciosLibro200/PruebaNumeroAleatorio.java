846183d9825f7caaed3f37c22057e9f7dbe31dd0
Javi <Javi@Javi-PC>
2020-03-02 19:30:15 +0100

    haciendo pull a mi eclipse

--- ejerciciosLibro200/PruebaNumeroAleatorio.java
+++
@@ -1,9 +0,0 @@
-package ejerciciosLibro200;
-
-public class PruebaNumeroAleatorio {
-public static void main(String[] args ) {
-	
-	NumeroAleatorio nu=new NumeroAleatorio();
-	nu.generaNumero();
-}
-}
